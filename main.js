a459d8d094290f50649738c17cc43de9f6517748
Igor <124437155+Pzqu@users.noreply.github.com>
2023-03-21 13:05:42 +0300

    ввввв

---
+++ main.js
@@ -0,0 +1,18 @@
+
+//объявление переменных
+
+//переменный в старом формате es5
+var lessonTopic = "Учим джскр";
+console.log(lessonTopic);
+
+//переменные в новом формате ES6
+let userName = "dima";
+console.log(userName);
+
+//переменный let может менять своё значение
+userName = "roma";
+console.log(userName);
+
+//переменный const неможет менять своё значение
+const  birthYear = 1990;
+console.log(birthYear );
